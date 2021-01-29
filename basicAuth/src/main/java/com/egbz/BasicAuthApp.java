89f2ccbf5c4a947174f3770a16522354eca82f53
egbz <1023271893@qq.com>
2021-01-29 17:26:05 +0800

    amend

---
+++ basicAuth/src/main/java/com/egbz/BasicAuthApp.java
@@ -0,0 +1,16 @@
+package com.egbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author egbz
+ * @date 2021/1/29
+ */
+@SpringBootApplication
+public class BasicAuthApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BasicAuthApp.class, args);
+        System.out.println("Violet");
+    }
+}
---
+++ src/main/java/com/egbz/authDemo/AuthDemoApplication.java
@@ -0,0 +1,14 @@
+package com.egbz.authDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthDemoApplication.class, args);
+		System.out.println("hi");
+	}
+
+}
